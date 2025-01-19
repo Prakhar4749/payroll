0c0f85c577a269e8bb7f6afd4aad2b42276e7ec6
GAVESH BATHAM <gaveshbatham1@gmail.com>
2025-01-20 02:01:37 +0530

    added api of full detail of emp , remove emp , add new emp

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,17 +5,8 @@ const PORT  = 5000
 const app = express();
 
 app.use(express.json())
+
 app.use('/emp' , emproute)
-
-
-
-
-
-
-
-
-
-
 
 
 
--- backend/src/routes/emp.js
+++ backend/src/routes/emp.js
@@ -1,16 +1,24 @@
 import express from 'express'
-import {get_all_emp_details ,get_e_id_emp_details} from '../controller/emp.js'
+import {get_all_basic__emp_details ,get_all_e_id_emp_details , delete_e_id ,add_new_emp } from '../controller/emp.js'
 
 
 const route = express.Router();
 
 
 //  for geting all the emp_details
-route.get("/",get_all_emp_details);
+route.get("/",get_all_basic__emp_details);
 
 
 //  for geting detail by e_id the emp_details
-route.get("/:e_id",get_e_id_emp_details);
+route.get("/:e_id",get_all_e_id_emp_details);
+
+
+// to delete the emp by the e_id from tables (emp_details, emp_bank_details,emp_deduction_details ,emp_earning_details)
+route.delete("/:e_id" , delete_e_id)
+
+// to add new emp to database ini tables (emp_details, emp_bank_details,emp_deduction_details ,emp_earning_details) 
+// post should be in formate { "emp_details": {...} , "emp_deduction_details": {...} ..... }
+route.post('/' , add_new_emp)
 
 
 export default route;
