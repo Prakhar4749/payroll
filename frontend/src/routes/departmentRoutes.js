86f8c4d4b637e5e68d5da51e44b35f1008b88cb7
Prakhar sakhare~ <prakharsakhare2226@gmail.com>
2025-01-22 18:56:45 +0530

    styntax error resolved

--- frontend/src/routes/departmentRoutes.js
+++ frontend/src/routes/departmentRoutes.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Department from '../views/Department';
-import dept_update_form from '../components/page_specific/dept_update_form.js';
+import Dept_update_form from '../components/page_specific/Dept_update_form.js';
 
 const departmentRoutes = [
   <Route key="department" path="/department" element={<Department />} />,
-  <Route key="department_update_form" path="/department/update_form" element={<dept_update_form/>} />,
+  <Route key="department_update_form" path="/department/update_form" element={<Dept_update_form/>} />,
 ];
 
 export default departmentRoutes;
