b22ec3f6fc5cb47b60d6375641468cd719601cc9
GAVESH BATHAM <gaveshbatham1@gmail.com>
2025-01-24 16:48:51 +0530

    pdf generater added but with issu

--- frontend/src/routes/payslipRoutes.js
+++ frontend/src/routes/payslipRoutes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Payslip from '../views/Payslip';
 import Payslip_Form from '../components/page_specific/Payslip_Form';
+import PayslipPdf from "../utils/PayslipPdf"
 
 const payslipRoutes = [
   <Route key="payslip" path="/payslip" element={<Payslip />} />,
   <Route key="payslip_form" path="/payslip/form" element={<Payslip_Form />} />,
+  <Route key="payslip_pdf" path="/payslip/payslipPdf" element={<PayslipPdf />} />,
 ];
 
 export default payslipRoutes;
